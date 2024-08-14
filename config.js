5964e1f9d3cfd85b7608c3456fe7b59a68b68511
Glitch (military-zenith-search) <none>
2024-08-14 20:05:55 +0000

    ⏰👗 Checkpoint ./config.js:69746079/12109

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	clientID: process.env.CLIENT_ID || '<88716649c105b584>',
 	clientSecret: process.env.CLIENT_SECRET || '<381b80e0bb2bf0969fa2fa2df1ff34d1a2cbafd8>',
-	callbackURL: process.env.CALLBACK_URL || '<https://iridescent-jagged-magic.glitch.me/auth/pipedrive/callback>' 
+	callbackURL: process.env.CALLBACK_URL || '<https://iridescent-jagged-magic.glitch.me/callback>' 
 };
