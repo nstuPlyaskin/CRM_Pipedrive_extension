78e9822e0db49aa1b2b0041e2722749bb2839533
Glitch (military-zenith-search) <none>
2024-08-15 11:49:38 +0000

    🖐🏬 Checkpoint ./index.js:69746079/7674

--- index.js
+++ index.js
@@ -18,6 +18,33 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Функция для проверки существования кастомного поля
+async function getOrCreateCustomField(fieldName, fieldType) {
+  try {
+    // Поиск поля по названию
+    const fieldsResponse = await axios.get(`${apiUrl}/dealFields?api_token=${apiToken}`);
+    const fields = fieldsResponse.data.data;
+
+    // Проверка существования поля
+    const existingField = fields.find(field => field.name === fieldName);
+
+    if (existingField) {
+      // Поле существует, возвращаем его ID
+      return existingField.id;
+    } else {
+      // Поле не существует, создаем его
+      const createFieldResponse = await axios.post(`${apiUrl}/dealFields?api_token=${apiToken}`, {
+        name: fieldName,
+        field_type: fieldType,
+      });
+      return createFieldResponse.data.data.id;
+    }
+  } catch (error) {
+    console.error(`Error in getting or creating custom field ${fieldName}:`, error.response ? error.response.data : error.message);
+    throw new Error(`Failed to get or create custom field: ${fieldName}`);
+  }
+}
+
 // GET-маршрут для отображения формы в iframe
 app.get('/callback', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'callback.html'));
@@ -28,6 +55,20 @@ app.post('/submit-job', async (req, res) => {
   const jobData = req.body;  // Данные из формы
 
   try {
+    // Создаем или находим кастомные поля
+    const jobTypeFieldId = await getOrCreateCustomField('Job Type', 'varchar');
+    const jobSourceFieldId = await getOrCreateCustomField('Job Source', 'varchar');
+    const jobDescriptionFieldId = await getOrCreateCustomField('Job Description', 'text');
+    const addressFieldId = await getOrCreateCustomField('Address', 'varchar');
+    const cityFieldId = await getOrCreateCustomField('City', 'varchar');
+    const stateFieldId = await getOrCreateCustomField('State', 'varchar');
+    const zipCodeFieldId = await getOrCreateCustomField('Zip Code', 'varchar');
+    const areaFieldId = await getOrCreateCustomField('Area', 'varchar');
+    const startDateFieldId = await getOrCreateCustomField('Start Date', 'date');
+    const startTimeFieldId = await getOrCreateCustomField('Start Time', 'time');
+    const endTimeFieldId = await getOrCreateCustomField('End Time', 'time');
+    const testSelectFieldId = await getOrCreateCustomField('Test Select', 'varchar');
+
     // Создаем нового клиента (или настраиваем существующего)
     let personId = null;
     if (jobData.email) {
@@ -54,25 +95,23 @@ app.post('/submit-job', async (req, res) => {
       }
     }
 
-    // Создаем новую сделку
+    // Создаем новую сделку с кастомными полями
     const dealResponse = await axios.post(`${apiUrl}/deals?api_token=${apiToken}`, {
       title: `${jobData.firstName} ${jobData.lastName} - ${jobData.jobType}`,
       person_id: personId,
       value: 0, // Установите значение сделки, если это необходимо
-      custom_fields: {
-        job_type: jobData.jobType,
-        job_source: jobData.jobSource,
-        job_description: jobData.jobDescription,
-        address: jobData.address,
-        city: jobData.city,
-        state: jobData.state,
-        zip_code: jobData.zipCode,
-        area: jobData.area,
-        start_date: jobData.startDate,
-        start_time: jobData.startTime,
-        end_time: jobData.endTime,
-        test_select: jobData.testSelect
-      }
+      [jobTypeFieldId]: jobData.jobType,
+      [jobSourceFieldId]: jobData.jobSource,
+      [jobDescriptionFieldId]: jobData.jobDescription,
+      [addressFieldId]: jobData.address,
+      [cityFieldId]: jobData.city,
+      [stateFieldId]: jobData.state,
+      [zipCodeFieldId]: jobData.zipCode,
+      [areaFieldId]: jobData.area,
+      [startDateFieldId]: jobData.startDate,
+      [startTimeFieldId]: jobData.startTime,
+      [endTimeFieldId]: jobData.endTime,
+      [testSelectFieldId]: jobData.testSelect
     });
 
     console.log('Deal Created:', dealResponse.data);
